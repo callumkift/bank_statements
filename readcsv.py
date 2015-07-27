4a3430ceb78ecd4cb5ae14e641eafb7d68c89106
Callum Kift <callumkift@gmail.com>
2015-07-27 17:39:35 +0200

    Added method that finds CSV files to read.

--- readcsv.py
+++ readcsv.py
@@ -3,13 +3,23 @@
 import os
 
 def findcsvfiles(path2files):
-
+    """
+    Method finds the CSV files to read and extract data.
+    :param path2files: Path to directory that contains CSV files
+    :return: A list of CSV files to read.
+    """
     csvfiles = []
 
     if os.path.isdir(path2files):
         for file in os.listdir(path2files):
             if file.endswith(".csv"):
-                csvfiles.append(path2files + "")
+                csvfiles.append(path2files + str(file))
+
+        if len(csvfiles) != 0:
+            return csvfiles
+        else:
+            print "Error: No CSV files found in given directory."
+            return
     else:
         print "Error: Directory does not exist."
-    return
+        return
