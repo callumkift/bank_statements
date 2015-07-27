48757e470dd1fbca0dd09b862f4c408bb54bf38a
Callum Kift <callumkift@gmail.com>
2015-07-27 18:10:24 +0200

    Added method thatextracts data from CSV files.

--- readcsv.py
+++ readcsv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import string
 
 def findcsvfiles(path2files):
     """
@@ -19,17 +20,41 @@ def findcsvfiles(path2files):
             print "CSV files returned"
             return csvfiles
         else:
-            print "Error: No CSV files found in given directory."
+            print "Error - findcsvfiles(path2files): No CSV files found in given directory."
             return
     else:
-        print "Error: Directory does not exist."
+        print "Error - findcsvfiles(path2files): Directory does not exist."
         return
+
 
 def readcsvfiles(csvlist):
 
     if len(csvlist) != 0:
         for i in range(len(csvlist)):
             print csvlist[i]
+            extractdata(csvlist[i])
     else:
-        print "Error: No CSV files in list. Are there CSV files in given directory?"
+        print "Error - readcsvfiles(csvlist): No CSV files in list. Are there CSV files in given directory?"
+        return
+
+
+def extractdata(csvfile):
+
+    csvdata = []
+
+    if os.path.exists(csvfile):
+        with open(csvfile, "r") as f:
+            f.readline()
+            f.readline()
+            for line in f:
+                line = line.strip()
+                column = line.split(";")
+                if len(column) == 5:
+                    csvdata.append([column[0], str(column[1]), column[2], float(string.replace(column[3], ",", ".")), float(string.replace(column[4], ",", "."))])
+                else:
+                    print "Error - extractdata(csvfile): Wrong number of columns. Check file format."
+        print csvdata[0]
+        return
+    else:
+        print "Error - extractdata(csvfile): Cannot find CSV file."
         return
