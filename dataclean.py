5f5980898ba3b8c77949f8dd7e560bcc1bfc093c
Callum Kift <callumkift@gmail.com>
2015-07-28 12:23:07 +0200

    Added method that removes reserved payments.

--- dataclean.py
+++ dataclean.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-def formatlist():
+def formatlist(form_list):
 
-    return
+    a = rem_res(form_list)
+
+    return a
+
+
+def rem_res(res_list):
+    """
+    Removes transactions from list that have not gone through - they are reserved.
+    :param res_list: A list of transactions.
+    :return: The transaction list with reserved payments removed.
+    """
+
+    res_elements = []
+
+    for i in range(len(res_list)):
+        if res_list[i][0] == "Reserveret":
+            res_elements.append(i)
+
+    res_elements.reverse()
+    # Reversed so that it deletes the correct elements
+
+    for j in range(len(res_elements)):
+        del res_list[res_elements[j]]
+
+    return res_list
+
 
 
 def cleanlist():
