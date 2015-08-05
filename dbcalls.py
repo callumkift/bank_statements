bdfdc86146dadcce7273fc185ac9e360a6e54613
Callum Kift <callumkift@gmail.com>
2015-08-05 21:01:27 +0200

    Method to add transcations to database started.

--- dbcalls.py
+++ dbcalls.py
@@ -21,7 +21,7 @@ def createdb():
         return
     else:
 
-        conn = sqlite3.connect(path2db)
+        conn = connect()
         c = conn.cursor()
 
         c.execute("CREATE TABLE TransactionType(id INTEGER PRIMARY KEY, type TEXT, UNIQUE(type))")
@@ -37,12 +37,24 @@ def createdb():
 
         print "check - db and tables created"
 
-        add_default_tt(path2db)
+        add_default_tt()
 
         return
 
 
-def add_default_tt(db):
+def connect():
+    """
+    Connects to database
+    :return: connection to sqlite database
+    """
+    dbname = "TransactionDB.sqlite"
+    path2dir = "/Users/callumkift/dev_projects/nordea_banking/"
+    path2db = path2dir + dbname
+
+    return sqlite3.connect(path2db)
+
+
+def add_default_tt():
     """
     Adds default values into TransactionType table
     :param db: Database to connect to
@@ -50,7 +62,7 @@ def add_default_tt(db):
     """
     d_tt = ["Supermarket", "ATM", "Kiosk", "Shopping", "Night Out", "Transport", "Money In",
             "House"]  # List of default transaction types
-    conn = sqlite3.connect(db)
+    conn = connect()
     c = conn.cursor()
 
     for i in range(len(d_tt)):
@@ -69,16 +81,46 @@ def add2db(trans_list):
     """
     nt = len(trans_list)
 
-    for i in range(1):
+    conn = connect()
+    c = conn.cursor()
+
+    for i in range(10):
         des = trans_list[i][1]
         amo = trans_list[i][3]
         bal = trans_list[i][4]
         dt = trans_list[i][5]
 
-        # print ""
-        # print des
-        # print amo
-        # print bal
-        # print dt
+        c.execute("SELECT * FROM TransactionPlace WHERE description = (?) LIMIT 1", (des,))
+
+        tp_return = c.fetchall()
+
+
+        if len(tp_return) == 0:
+            print "No description match for %s" % des
+        else:
+            tt_id = tp_return[0][1]
+            print "Match\n", tt_id
+
+            # check if transaction exists
+
+            c.execute('''SELECT * FROM TransactionInfo
+                            WHERE date = (?)
+                            AND amount = (?)
+                            AND balance = (?)''', (dt,amo, bal, ))
+
+            ti_return = c.fetchall()
+
+            if len(ti_return) == 0:
+                c.execute("INSERT INTO TransactionPlace(tt_id, description) VALUES(?,?)", (tt_id, des, ))
+                conn.commit()
+                c.execute("SELECT id FROM TransactionPlace WHERE id = (SELECT MAX(id) FROM TransactionPlace)")
+                tp_id = c.fetchall()[0][0]
+                c.execute('''INSERT INTO TransactionInfo(tt_id, tp_id, date, amount, balance)
+                                VALUES(?,?,?,?,?)''', (tt_id, tp_id, dt, amo, bal, ))
+                conn.commit()
+            else:
+                print "Transaction already exists."
+
+    conn.close()
 
     return
