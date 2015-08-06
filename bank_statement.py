a324017f78af1eb10dc51656fd9e32711ef0a304
Callum Kift <callumkift@gmail.com>
2015-08-06 17:39:03 +0200

    Transaction details are added to database.

--- bank_statement.py
+++ bank_statement.py
@@ -26,11 +26,38 @@
 
 import readcsv
 import dataclean
+import dbcalls as dbc
+import sqlite3
 
 if __name__ == '__main__':
     path2csv = "/Users/callumkift/dev_projects/"
+
+    dtt = ["Supermarket", "ATM", "Kiosk", "Shopping", "Night Out", "Food", "Transport", "Money In",
+                           "House", "Rent and Bills", "Hobbies", "Misc"]  # List of default transaction types
+
+    dbc.createdb(dtt)
 
     csvfiles = readcsv.findcsvfiles(path2csv)  # List of paths to CSV files
     translist = readcsv.readcsvfiles(csvfiles)  # List of transactions from all CSV files
 
     cleanlist = dataclean.formatlist(translist)  # Cleans up the transaction data
+
+    dbc.add2db(cleanlist)
+
+    ### TEST
+
+    dbname = "TransactionDB.sqlite"
+    path2dir = "/Users/callumkift/dev_projects/nordea_banking/"
+    path2db = path2dir + dbname
+
+    conn = sqlite3.connect(path2db)
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM TransactionInfo")
+    stuff = c.fetchall()
+    conn.close()
+
+    stuff_added = len(stuff)
+
+    print "Number of transactions read from CSVs: %d" % len(cleanlist)
+    print "Number of transactions in DB: %d" % stuff_added
