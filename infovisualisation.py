01852852c4b764383e0e4e095eedfb195e2590a2
Callum Kift <callumkift@gmail.com>
2015-08-10 11:13:44 +0200

    Added a plot to compare the types of spending per weekday.

--- infovisualisation.py
+++ infovisualisation.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
 import dbcalls as dbc
+import matplotlib.cm as cm
 
 
 def generalview():
@@ -30,7 +31,7 @@ def generalview():
 
         trans_data_dict[key] = [abs(month_amount), abs(total_amount)]
 
-    # Making Pie Chart
+    # Making Bar Chart
     labels = []
     mspend = []
     tspend = []
@@ -50,20 +51,90 @@ def generalview():
     fig, axarr = plt.subplots(1, 2)
 
     ind = np.arange(len(labels))
-    width = 0.8
+    width = 0.8  # width of bar
+    rot = 75  # Angle of text (x-axis)
 
     axarr[0].bar(ind, mspend, color="red")
     axarr[0].set_xticks(ind + (width / 2))
-    axarr[0].set_xticklabels(labels, rotation=45)
+    axarr[0].set_xticklabels(labels, rotation=rot)
     axarr[0].set_ylabel("Amount Spent DKK")
     axarr[0].set_title("Monthly Spend - (Net In: %.2f DKK)" % (m_in - np.sum(mspend)))
 
     axarr[1].bar(ind, tspend, color="green")
     axarr[1].set_xticks(ind + (width / 2))
-    axarr[1].set_xticklabels(labels, rotation=45)
+    axarr[1].set_xticklabels(labels, rotation=rot)
     axarr[1].set_ylabel("Amount Spent DKK")
     axarr[1].set_title("Total Spend - (Net In: %.2f DKK)" % (t_in - np.sum(tspend)))
 
     plt.suptitle("Where Money is Spent")
     plt.show()
+    return
+
+
+def dayview():
+    trans_data_dict = dbc.getgeneral()
+
+    # Changes dictionary items to key = [monday_spend, tuesday_spend, wednesday_spend, etc.]
+    for key, value in trans_data_dict.iteritems():
+        vdate, vamount = zip(*value)
+        mon_sum = 0
+        tue_sum = 0
+        wed_sum = 0
+        thu_sum = 0
+        fri_sum = 0
+        sat_sum = 0
+        sun_sum = 0
+
+        for i in range(len(vdate)):
+            date = dt.datetime.strptime(vdate[i], "%Y-%m-%dT%H:%M:%S")
+
+            if date.weekday() == 0:
+                mon_sum += vamount[i]
+            elif date.weekday() == 1:
+                tue_sum += vamount[i]
+            elif date.weekday() == 2:
+                wed_sum += vamount[i]
+            elif date.weekday() == 3:
+                thu_sum += vamount[i]
+            elif date.weekday() == 4:
+                fri_sum += vamount[i]
+            elif date.weekday() == 5:
+                sat_sum += vamount[i]
+            elif date.weekday() == 6:
+                sun_sum += vamount[i]
+
+        trans_data_dict[key] = [abs(mon_sum), abs(tue_sum), abs(wed_sum), abs(thu_sum), abs(fri_sum), abs(sat_sum),
+                                abs(sun_sum)]
+
+    # Variables needed for graph
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]  # x-axis tick labels
+    no_of_types = len(trans_data_dict.keys())  # number of transaction types
+    ind = np.arange(len(days))  # index
+    day_width = 0.95  # so that there is a gap between days
+    width = day_width / no_of_types  # width of each type's bar
+    colours = iter(cm.rainbow(np.linspace(0, 1, no_of_types)))  # Allows to automatically assign colours
+    rot = 0  # Angle of text (x-axis)
+    count = 0
+
+    # Plotting
+    # fig = plt.figure()
+    ax = plt.subplot(111)
+    for key, values in trans_data_dict.iteritems():
+        if key != "Money In":
+            ax.bar(ind + (count * width), values / np.sum(values), width, color=next(colours), label=key)
+            count += 1
+    ax.set_xticks(ind + (day_width / 2))
+
+    ax.set_xticklabels(days, rotation=rot)
+    ax.set_xlabel("Day of the Week")
+    ax.set_ylabel("Normalised Spending")
+    ax.set_title("Comparison Of Spending Type Per Day")
+
+    # Shrink plot width to put legend outside
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.95, box.height])
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    plt.show()
+
     return
